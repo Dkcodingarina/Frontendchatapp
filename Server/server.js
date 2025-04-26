c280e84473fa592f6be6ba5723894fe989a47adf
DEEPANSHU <139827048+Dkcodingarina@users.noreply.github.com>
2025-04-26 15:08:05 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 //Socket
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://frontendchatapp-pi.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
